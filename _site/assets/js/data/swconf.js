7975df0d2e638d6e3779bd5471b7847f4fcd903a
Nguyễn Văn Mỹ <ngvanmy114@gmail.com>
2025-03-23 21:47:15 +0700

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742720841',resources: [
+    cacheName: 'chirpy-1742741232',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
